a1f4f884c9bfb3c121cdd22f22933297fd0e22e6
ted.song <songsungkyun@gmail.com>
2016-03-21 18:25:05 +0900

    .

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+rm -rf /Users/songsungkyun/Desktop/dev/intellij/workspace/uuidcode.github.io/alphabet
 cp -R ../src/main/webapp /Users/songsungkyun/Desktop/dev/intellij/workspace/uuidcode.github.io/alphabet
